3bb595da5fd8c99c9f32c92df17819664a97c395
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-06-17 23:04:02 +0200

    feat(server): create server with database initialization and basic endpoint
    - Created server with express
    - Configured dotenv to load environment variables based on NODE_ENV
    - Set up mongoose connection to MongoDB
    - Implemented initialDataBase to initialize the database
    - Added basic endpoint '/' to respond with 'Hello, World!'
    - Handled server startup and database connection errors

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import 'dotenv/config';
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import * as mongoose from 'mongoose';
+import * as process from 'process';
+import { initialDataBase } from './startup/initialDataBase.js';
+const envFile =
+  process.env.NODE_ENV === 'production'
+    ? '.env.production'
+    : '.env.development';
+dotenv.config({ path: envFile });
+
+const app: express.Application = express();
+
+const PORT: number = parseInt(process.env.PORT || '8080', 10);
+
+app.get('/', (req: Request, res: Response) => {
+  console.log('req', req);
+  res.send('Hello, World!');
+});
+async function start(): Promise<void> {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING || '');
+    console.log('Database connected');
+    await initialDataBase();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+start();
