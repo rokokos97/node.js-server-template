843e245dfe8860835486cd465c58862892e11443
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-06-17 18:46:57 +0200

    refactor(models): replace password field with age in user model
    - Removed password field from user.ts
    - Added age field to user.ts
    - Updated related logic and documentation to reflect the changes

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,11 +1,11 @@
 import { Schema, model } from 'mongoose';
-import {IUser} from "../interfaces";
+import { IUser } from '../interfaces';
 
 const userSchema = new Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    age: { type: String },
   },
   {
     timestamps: true,
