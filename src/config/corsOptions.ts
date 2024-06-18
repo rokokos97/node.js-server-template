ad57cd457c739d525cf54a146bfd4cc95a06fc20
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-06-18 15:03:33 +0200

    feat(config): create corsOptions configuration
    - Created corsOptions.ts in the config directory with CORS settings
    - Imported and applied CORS settings in the server file (index.ts)

---
+++ src/config/corsOptions.ts
@@ -0,0 +1,10 @@
+import { CorsOptions } from 'cors';
+
+const corsOptions: CorsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+export default corsOptions;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import * as mongoose from 'mongoose';
 import * as process from 'process';
 import { initialDataBase } from './startup/initialDataBase.js';
 import router from './routes/index.js';
+import corsOptions from './config/corsOptions';
 const envFile =
   process.env.NODE_ENV === 'production'
     ? '.env.production'
@@ -14,6 +16,7 @@ dotenv.config({ path: envFile });
 const app: express.Application = express();
 const PORT: number = parseInt(process.env.PORT || '8080', 10);
 
+app.use(cors(corsOptions));
 app.use('/api', router);
 
 app.get('/', (req: Request, res: Response) => {
